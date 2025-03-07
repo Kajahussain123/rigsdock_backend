20b924bd309527b27d2d4b078e5e0046c977f0ef
bijithkk <informbijith@gmail.com>
2025-03-07 12:45:58 +0530

    updated

--- controllers/Admin/User/UserController.js
+++ controllers/Admin/User/UserController.js
@@ -3,7 +3,7 @@ const User = require('../../../models/User/AuthModel');
 // get all users
 exports.getAllUsers = async(req,res) => {
     try {
-        const users = await User.find().select('-password');
+        const users = await User.find().select('-password').sort({createdAt: -1});
         if(users.length === 0) {
             return res.status(404).json({ message: "No users found" });
         }
