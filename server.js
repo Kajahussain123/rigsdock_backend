c0c092c9744d489d150bf12948050f341af5cd33
Abijithkk <kkabhijith59@gmail.com>
2025-06-26 12:51:07 +0530

    latest

--- server.js
+++ server.js
@@ -271,7 +271,6 @@ socketHandler(io);
 
 const PORT = process.env.PORT || 3006;
 
-server.listen(PORT, () => {
-    console.log(`Server started listening at PORT ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
- 
